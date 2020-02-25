bee85daf71fe519a0609445d97d2db760824e205
Tomáš Rovder <tomasrovder@gmail.com>
2020-02-25 12:43:38 +0100

    Updates

---
+++ precache-manifest.b24fa50f47aac80f0d5021cdb4617b64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070b3bc9b4c6f8cf063d7971d1a09139",
+    "url": "/rewear-frontend/index.html"
+  },
+  {
+    "revision": "71ede4a52d88e4d8b402",
+    "url": "/rewear-frontend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9f6a1aadca7794e406db",
+    "url": "/rewear-frontend/static/js/2.74f1c8ea.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rewear-frontend/static/js/2.74f1c8ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ede4a52d88e4d8b402",
+    "url": "/rewear-frontend/static/js/main.c941e5aa.chunk.js"
+  },
+  {
+    "revision": "eb99acd019ec8ec540d9",
+    "url": "/rewear-frontend/static/js/runtime-main.ed197840.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rewear-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
